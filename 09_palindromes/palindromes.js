1f89ec6d84c9cc77f1f114fabe620a91cc6f7d65
Joseph Loar <enginma2@gmail.com>
2022-06-17 14:55:21 -0500

    completed palindromes function, tests good

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,18 @@
-const palindromes = function () {
+const palindromes = function (givenString) {
+  let cleaned = givenString.replace(/[^a-z0-9]/gi, ''); // regex to remove any non-alphanumeric characters
+  let testString = cleaned.toLowerCase();
+  stringLength = testString.length;
+  fromEnd = stringLength - 1;
+  for(i=0;i<stringLength;i++){
+    if(testString[i] == testString[fromEnd]){
+      fromEnd--;
+      continue;
+    }else{
+      return false;
+    }
 
+  }
+  return true;
 };
 
 // Do not edit below this line
