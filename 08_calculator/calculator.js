717601342245218dc246a471f268fc873e30bcdf
Joseph Loar <enginma2@gmail.com>
2022-06-17 13:09:44 -0500

    completed math functions

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function(numberGiven1,numberGiven2) {
+  return (numberGiven1 + numberGiven2);
 };
 
-const subtract = function() {
-	
+const subtract = function(numberGiven1,numberGiven2) {
+  return (numberGiven1 - numberGiven2);
 };
 
-const sum = function() {
-	
+const sum = function(numbersGiven) {
+  total = 0;
+  aLength = numbersGiven.length;
+  for(i=0;i<aLength;i++){
+    total+=numbersGiven[i];
+  };
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function(numbersGiven) {
+  let givenLength = numbersGiven.length;
+  let currentNumber = 1;
+  for( i = 0; i < givenLength; i++){
+    currentNumber = currentNumber * numbersGiven[i];
+  }
+  return currentNumber;
 };
 
-const power = function() {
-	
+const power = function(numberGiven, powerOf) {
+  return numberGiven ** powerOf;
 };
 
-const factorial = function() {
-	
+const factorial = function(numberGiven) {
+  if (numberGiven < 0){
+    return "ERROR";
+  }else if(numberGiven == 0){
+    return 1;
+  }else{
+    return (numberGiven * factorial(numberGiven - 1));
+  }
 };
 
 // Do not edit below this line
