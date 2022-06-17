71074943e4ec14f8e568891d10e3df4238bc1275
Joseph Loar <enginma2@gmail.com>
2022-06-17 11:44:15 -0500

    completed conversion functions

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,22 @@
-const ftoc = function() {
-
+const ftoc = function(farenheit) {
+  let middle = (farenheit - 32 ) * 5 / 9;
+  let result = mRound(middle);
+  return result;
 };
 
-const ctof = function() {
+const ctof = function(celsius) {
+  let middle = celsius * 9/5 + 32;
+  let result = mRound(middle);
+  return result;
+};
 
+function mRound(notRoundNumber){
+  return (Math.round( notRoundNumber * 10 ) / 10);
+};
+
+function fRound(notRoundNumber){
+  let desiredNumber = parseFloat(notRoundNumber.toFixed(1));
+  return desiredNumber;
 };
 
 // Do not edit below this line
